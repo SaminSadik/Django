2362b767c4184bd5f82ecfbaecf5e1a33f86a00e
SaminSadik <Saminsadikkhan@gmail.com>
2024-03-17 17:00:45 +0600

    new

--- venv_1/project_1/project_1/views.py
+++ venv_1/project_1/project_1/views.py
@@ -4,6 +4,5 @@ from django.http import HttpResponse
 
 def contact(request):
     return HttpResponse("This is the contact page")
-
 def homepage(request):
     return HttpResponse("This is the homepage")
