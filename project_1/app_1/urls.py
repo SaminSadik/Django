7d13a57f4ebc8328f3a8584510800beb32554072
SaminSadik <Saminsadikkhan@gmail.com>
2024-03-25 01:45:32 +0600

    updated

--- project_1/app_1/urls.py
+++ project_1/app_1/urls.py
@@ -5,4 +5,5 @@ from . import views # for using functions from app views.py
 
 urlpatterns = [
     path('', views.home), # accessible from projectURL/appName/
+    path('temp/', views.template), # accessible from projectURL/appName/temp/
 ]
--- project_1/app_1/views.py
+++ project_1/app_1/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 # Create your views here.
 
 def home(request):
     return render(request, "app_1/appHome.html") # for loading html page
+
+def template(request):
+    # Context (passing something from backend to frontend):
+    d = {'Name' : 'San', 'Age' : 21,
+         'Dict' : [
+             {'id':1, 'Title':'Python'},
+             {'id':2, 'Title':'HTML'}
+            ],
+         'Datetime' : datetime.datetime.now(),
+         'Birthday' : datetime.date(year=2003, month=3, day=20)
+        }
+    # can be passed as an key-value pair (dictionary)
+    return render(request, "app_1/template.html", d) # passing context
+    # return render(request, "app_1/template.html", context={'Name' : 'San', 'Age' : 21})
