54e4a66bb4325d195201477b34e243b393692a2d
Samin Sadik Khan <124756396+SaminSadik@users.noreply.github.com>
2024-05-28 02:17:02 +0600

    Deleted Project

--- week5_module20_midterm/brands/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . import models
-
-# Register your models here.
-class Brand_Admin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)} 
-    list_display = ['name']
-
-admin.site.register(models.BrandModel, Brand_Admin)
