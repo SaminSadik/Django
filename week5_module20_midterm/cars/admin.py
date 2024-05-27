c197d884d8ad250a64d69852fb99e3954bf1823d
SaminSadik <Saminsadikkhan@gmail.com>
2024-05-28 02:43:32 +0600

    Deleted

--- week5_module20_midterm/cars/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from . import models
-
-# Register your models here.
-class Car_Admin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)} 
-    list_display = ['name']
-
-admin.site.register(models.CarModel, Car_Admin)
--- week5_module20_midterm/cars/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('<slug:car_slug>/', views.CarDetails.as_view(), name='car_details')
-]
